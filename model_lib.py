5e1989ab75d89752578647458f31a16bb1dcb3e5
aravind.nujella <aravind.nujella@gmail.com>
2018-05-21 23:18:22 +0530

    much faster model; need to implement mask loss weighting

--- model_lib.py
+++ model_lib.py
@@ -29,42 +29,54 @@ class CocoDataset(torch.utils.data.Dataset):
         self.class_ids = range(config.NUM_CLASSES)
         # class_wise_iterators
         self.cw_iter = [0 for i in range(config.NUM_CLASSES)]
-    # regardless of instance_index, we give some shit. 
+        self.sampler = self.weighted_sampler()
+
+
+    # regardless of instance_index, we give some shit.
     # shouldn't matter anyway because of blah blah
     def __getitem__(self, instance_index):
-        impulse, gt_response, is_bad_image = None, None, True
-        class_id = random.choice(self.class_ids)
         # skipping bad images. is this bad?
-        while is_bad_image:
-            image, masks, class_id = self.load_image_gt(class_id,self.cw_iter[class_id])
-            # image -= self.config.MEAN_PIXEL
-            impulse, gt_response, class_id, is_bad_image = self.generate_targets(
-                masks, class_id)
+        # caution: cw dataset didnt note if its a crowd. needs fixing
+        while True:
+            class_id = next(self.sampler)
+            self.cw_iter[class_id] = (self.cw_iter[class_id] + 1) % self.cw_num_instances[class_id]
+            image, masks, class_id = self.load_image_gt(class_id, self.cw_iter[class_id])
+            impulse, gt_response, class_id, is_bad_image = self.generate_targets(masks, class_id)
             if not is_bad_image:
                 # channels first
-                image = np.moveaxis(image, 2, 0).astype(np.float32)
+                image = image.astype(np.float32)
+                # image -= self.config.MEAN_PIXEL
+                # image = image / 128
+                image = np.moveaxis(image, 2, 0)
                 impulse = np.moveaxis(np.expand_dims(impulse, -1), 2, 0).astype(np.float32)
                 gt_response = np.moveaxis(np.expand_dims(gt_response, -1), 2, 0).astype(np.float32)
-                self.cw_iter[class_id] = (self.cw_iter[class_id] + 1)%self.cw_num_instances[class_id]
                 return torch.from_numpy(image), torch.from_numpy(impulse), torch.from_numpy(gt_response), torch.tensor(
-            class_id,dtype=torch.long)
-            self.cw_iter[class_id] = (self.cw_iter[class_id] + 1)%self.cw_num_instances[class_id]
+                        class_id, dtype=torch.long)
 
     def __len__(self):
         return len(self.dataset.instance_info)
 
+    def weighted_sampler(self):
+        # TODO: repeatedly take am between median and freq
+        class_weighting = np.array([1 / self.config.NUM_CLASSES for i in range(self.config.NUM_CLASSES)])
+        reweighting = np.array(self.cw_num_instances)
+        # adjust number of bg instances reweighting
+        reweighting[0] = np.median(reweighting)
+        # reweighting = reweighting/np.sum(reweighting)
+        reweighting = reweighting**0.4
+        class_weighting = np.multiply(class_weighting, reweighting)
+        class_weighting = class_weighting / np.sum(class_weighting)
+        while True:
+            yield np.random.choice(self.class_ids, p=class_weighting)
+
     def generate_targets(self, masks, class_id):
         num_classes = self.config.NUM_CLASSES
-        mask = masks[:, :, 0]*128
-        umask = masks[:, :, 1]*128
+        mask = masks[:, :, 0]
+        umask = masks[:, :, 1]
         # what other bad cases? add them here
-        # currently crowd, zero sized masks are flagged as bad instances
-        if class_id < 0 or np.sum(mask) == 0:
+        # currently crowd, small sized masks are flagged as bad instances
+        if class_id < 0 or np.sum(mask) < 256:
             return None, None, None, True
-        # not bad image
-        # one_hot_class = np.zeros((num_classes,)).astype(np.int64)
-        # one_hot_class[class_id] = 1
-        # happens when cake on dining table, tie on human etc
         if np.sum(umask) / np.sum(mask) < 0.3:
             umask = mask
         # currently impulses are produced to fine tune for classification.
@@ -121,7 +133,7 @@ class CocoDataset(torch.utils.data.Dataset):
         config = self.config
         dataset = self.dataset
         # dont train on unlucky images. they are bad omen and model wont converge
-        cwid = cwid%self.cw_num_instances[class_id]
+        cwid = cwid % self.cw_num_instances[class_id]
         instance_info = dataset.class_wise_instance_info[class_id][cwid]
         image_path = instance_info["image_path"]
         mask_obj = instance_info["mask_obj"]
@@ -139,14 +151,15 @@ class CocoDataset(torch.utils.data.Dataset):
         # if random.random() > 0.5:
         #     image = np.fliplr(image)
         #     masks = np.fliplr(masks)
-        return image.astype(np.float32), masks, class_id
+        return image, masks, class_id
 
 
-# write custom collate to delete bad instances?
+# write custom collate to delete bad instances? 
+# not doing this because some times we might get all bad images
+# batch = list of (images,impulse,gt_response,class_id)
 # def _collate_fn(batch):
 #     batch = filter(lambda x: x is not None, batch)
-#     return default_collate(batch)
-
+#     return torch.utils.data.dataloader.default_collate(list(batch))
 # we take cid object from main (ugly interface)
 
 
@@ -154,35 +167,38 @@ def get_loader(dataset_cid, config):
     coco_dataset = CocoDataset(dataset_cid, config)
     data_loader = torch.utils.data.DataLoader(dataset=coco_dataset,
                                               batch_size=config.BATCH_SIZE,
-                                              # shuffle=True,
-                                              num_workers=8)
+                                              # collate_fn=_collate_fn,
+                                              shuffle=True,
+                                              num_workers=0)
     return data_loader
 
 # 1) convolve 2) batchnorm 3) add residual
 
+
 class BasicBlock(nn.Module):
-    def __init__(self,in_planes,mid_planes,out_planes,fr=(3,3)):
+
+    def __init__(self, in_planes, mid_planes, out_planes, fr=(3, 3)):
         # in_planes are mapped to out_planes by a bottleneck like connection,
-        # mid_planes only get 3,3 kernel size convs -> used to control no. of parms 
+        # mid_planes only get 3,3 kernel size convs -> used to control no. of parms
         # if more feature mixing is needed, use more of these
         super(BasicBlock, self).__init__()
-        pad = ((fr[0]-1)//2,(fr[1]-1)//2)
-        self.conv1 = nn.Conv2d(in_planes,mid_planes,kernel_size=(1,1))
+        pad = ((fr[0] - 1) // 2, (fr[1] - 1) // 2)
+        self.conv1 = nn.Conv2d(in_planes, mid_planes, kernel_size=(1, 1))
         self.bn1 = nn.BatchNorm2d(mid_planes)
-        self.conv2 = nn.Conv2d(mid_planes,mid_planes,kernel_size = fr,padding = pad)
+        self.conv2 = nn.Conv2d(mid_planes, mid_planes, kernel_size=fr, padding=pad)
         self.bn2 = nn.BatchNorm2d(mid_planes)
-        self.conv3 = nn.Conv2d(mid_planes,out_planes,kernel_size=(1,1))
+        self.conv3 = nn.Conv2d(mid_planes, out_planes, kernel_size=(1, 1))
         self.bn3 = nn.BatchNorm2d(out_planes)
         # highway is a direct linear transform on input to match output dimensions
-        self.highway = nn.Conv2d(in_planes,out_planes,kernel_size=(1,1))
+        self.highway = nn.Conv2d(in_planes, out_planes, kernel_size=(1, 1))
 
-    def forward(self,x):
+    def forward(self, x):
         # residual = self.highway(x)
 
         x = self.conv1(x)
         x = self.bn1(x)
         x = F.relu(x)
-        
+
         x = self.conv2(x)
         x = self.bn2(x)
         x = F.relu(x)
@@ -201,7 +217,7 @@ class BasicBlock(nn.Module):
 
 class MaskProp(nn.Module):
 
-    def __init__(self,n_dims):
+    def __init__(self, n_dims):
         super(MaskProp, self).__init__()
         self.bb1 = BasicBlock(n_dims, 32, 16)
         self.bb2 = BasicBlock(16, 8, 8)
@@ -218,15 +234,15 @@ class MaskProp(nn.Module):
 
 class Classifier(nn.Module):
 
-    def __init__(self,n_dims):
+    def __init__(self, n_dims):
         super(Classifier, self).__init__()
-        self.bb1 = BasicBlock(n_dims,64,64)
-        self.gap = nn.AvgPool2d((10, 10), stride=1)
+        self.bb1 = BasicBlock(n_dims, 64, 64)
+        self.gap = nn.AvgPool2d((7, 7), stride=1)
         self.fc = nn.Linear(64, 81)
 
     def forward(self, x):
         x = self.bb1(x)
-        x = F.max_pool2d(x, (2, 2), 2)
+        # x = F.max_pool2d(x, (2, 2), 2)
         x = self.gap(x)
         x = x.view(-1, 64)
         x = self.fc(x)
@@ -238,15 +254,14 @@ class SimpleHGModel(nn.Module):
 
     def __init__(self):
         super(SimpleHGModel, self).__init__()
-        cur_filters = 16
-        self.inp_conv_0 = nn.Conv2d(4, cur_filters, (7, 7), padding=(3,3))
+        self.inp_conv_0 = nn.Conv2d(4, 16, (7, 7), padding=(3, 3))
         # output dimensions
-        down_filter_sizes = [16,32,64,64,64,64]
-        bottleneck_filter_sizes = [16,32,64,64,64,64]
+        down_filter_sizes = [16, 32, 64, 64, 64, 64]
+        bottleneck_filter_sizes = [16, 32, 64, 64, 64, 64]
         # wing_filter_sizes = [None,None,16,32,256,256]
-        up_filter_sizes = [64,64,64,64]
+        up_filter_sizes = [64, 64, 64, 64]
 
-        self.down_conv_6 = BasicBlock(cur_filters, 64, down_filter_sizes[-6])
+        self.down_conv_6 = BasicBlock(16, 64, down_filter_sizes[-6])
 
         self.down_conv_5 = BasicBlock(down_filter_sizes[-6], 64, down_filter_sizes[-5])
 
@@ -290,9 +305,9 @@ class SimpleHGModel(nn.Module):
         # for m in self.modules():
         #     if isinstance(m, nn.Conv2d):
         #         nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            # elif isinstance(m, nn.BatchNorm2d):
-            #     nn.init.constant_(m.weight, 128)
-            #     nn.init.constant_(m.bias, 0)
+        # elif isinstance(m, nn.BatchNorm2d):
+        #     nn.init.constant_(m.weight, 128)
+        #     nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         # HourGlass
@@ -303,22 +318,22 @@ class SimpleHGModel(nn.Module):
         inp = self.inp_conv_0(inp)
         wing_convs = []
         # 6,6,8->6,6,16->5,5,16
-        inp = self.down_conv_6(inp); # wing_convs.append(self.wing_conv_6(inp)); 
+        inp = self.down_conv_6(inp);  # wing_convs.append(self.wing_conv_6(inp));
         inp = F.max_pool2d(inp, (2, 2), 2)
         # 5,5,16->5,5,32->4,4,32
-        inp = self.down_conv_5(inp); # wing_convs.append(self.wing_conv_5(inp)); 
+        inp = self.down_conv_5(inp);  # wing_convs.append(self.wing_conv_5(inp));
         inp = F.max_pool2d(inp, (2, 2), 2)
         # 4,4,32->4,4,64->3,3,64
-        inp = self.down_conv_4(inp); # wing_convs.append(self.wing_conv_4(inp)); 
+        inp = self.down_conv_4(inp);  # wing_convs.append(self.wing_conv_4(inp));
         inp = F.max_pool2d(inp, (2, 2), 2)
         # 3,3,64->3,3,128->2,2,128
-        inp = self.down_conv_3(inp); #wing_convs.append(self.wing_conv_3(inp)); 
+        inp = self.down_conv_3(inp);  # wing_convs.append(self.wing_conv_3(inp));
         inp = F.max_pool2d(inp, (2, 2), 2)
         # 2,2,128->2,2,256->1,1,256
-        inp = self.down_conv_2(inp);# wing_convs.append(self.wing_conv_2(inp)); 
+        inp = self.down_conv_2(inp); # wing_convs.append(self.wing_conv_2(inp));
         inp = F.max_pool2d(inp, (2, 2), 2)
         # 1,1,256->1,1,512->0,0,512
-        inp = self.down_conv_1(inp); wing_convs.append(self.wing_conv_1(inp)); 
+        inp = self.down_conv_1(inp); wing_convs.append(self.wing_conv_1(inp));
         # inp = F.max_pool2d(inp, (2, 2), 2)
         # 0,0,512->0,0,512
         # inp = self.mid_conv_0(inp);
@@ -327,7 +342,7 @@ class SimpleHGModel(nn.Module):
         # inp = F.upsample(inp, scale_factor=2); inp = self.up_conv_1(inp + wing_convs[-1]); # up_convs.append(inp)
         # # 2,2,128<-2,2,256<-1,1,256
         # inp = F.upsample(inp, scale_factor=2); inp = self.up_conv_2(inp + wing_convs[-2]); # up_convs.append(inp)
-        # # 3,3,64<-3,3,128<-2,2,128        
+        # # 3,3,64<-3,3,128<-2,2,128
         # inp = F.upsample(inp, scale_factor=2); inp = self.up_conv_3(inp + wing_convs[-3]); # up_convs.append(inp)
         # 4,4,32<-4,4,64<-3,3,64
         # inp = F.upsample(inp, scale_factor=2); inp = self.up_conv_4(inp + wing_convs[-4]); # up_convs.append(inp)
@@ -337,7 +352,7 @@ class SimpleHGModel(nn.Module):
         # inp = F.upsample(inp, scale_factor=2); inp = self.up_conv_6(inp + wing_convs[-6]); # up_convs.append(inp)
 
         # Maskprediction, classification
-        return self.class_predictor(wing_convs[-1]) # F.upsample(self.mask_predictor(inp),scale_factor = 8)
+        return self.class_predictor(wing_convs[-1])  # F.upsample(self.mask_predictor(inp),scale_factor = 8)
 
 
 def loss_criterion(gt_mask, pred_mask, gt_class, pred_class):
@@ -346,8 +361,8 @@ def loss_criterion(gt_mask, pred_mask, gt_class, pred_class):
     # else:
     #     return mask_loss(gt_mask,pred_mask) + classification_loss(gt_class,pred_class)
     # return mask_loss(gt_mask, pred_mask) #+ classification_loss(gt_class, pred_class)
-    # print(gt_class,pred_class)
-    return classification_loss(gt_class,pred_class)
+    return classification_loss(gt_class, pred_class)
+
 
 def mask_loss(gt_mask, pred_mask):
     # need to modify this
